a1906daa6039d91fb754692ce2ec3332f60d9430
Goldi <gold.1764i@gmail.com>
2025-07-05 16:48:10 +0530

    update date format

--- utils/formatData.js
+++ utils/formatData.js
@@ -11,7 +11,7 @@ const formatData = ( records ) => {
 		city: r.city,
 		station: r.station,
 		pollutant_id: r.pollutant_id,
-		last_update: r.last_update,
+		last_update: new Date(r.last_update),
 		latitude: parseFloat(r.latitude),
 		longitude: parseFloat(r.longitude),
 		min_value: parseFloat(r.min_value),
